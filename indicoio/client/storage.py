61171bcf1e197205040191645ed09efafaa167f1
jerry <jerry.genser@indico.io>
2020-02-22 11:37:30 -0500

    use name of file rather than file_idx

--- indicoio/client/storage.py
+++ indicoio/client/storage.py
@@ -5,7 +5,7 @@ from typing import List
 class StorageClient(RequestProxy):
     def upload_files(self, large_document_paths: List[str]):
         files = {
-            f"file_{idx}": open(file, "rb")
-            for idx, file in enumerate(large_document_paths)
+            file: open(file, "rb")
+            for file in enumerate(large_document_paths)
         }
         return self.post("/api/storage/files/upload?upload_type=user", files=files)
