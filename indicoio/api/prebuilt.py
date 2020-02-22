55bf9ff5c9bdfdb16902c60b83beab26ef00cca1
jerry <jerry.genser@indico.io>
2020-02-22 10:09:23 -0500

    uncomment get job results

--- indicoio/api/prebuilt.py
+++ indicoio/api/prebuilt.py
@@ -130,10 +130,10 @@ class IndicoApi(Indico):
                 """
             )
 
-        # job_id = response["data"]["documentExtraction"]["jobId"]
-        # job = self.build_object(JobResult, id=job_id)
-        # if job_results:
-        #     return job
-        # else:
-        #     job.wait()
-        #     return job.result()
+        job_id = response["data"]["documentExtraction"]["jobId"]
+        job = self.build_object(JobResult, id=job_id)
+        if job_results:
+            return job
+        else:
+            job.wait()
+            return job.result()
