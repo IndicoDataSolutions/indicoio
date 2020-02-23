27748abef6a625134f244dc59c955543096dd3bc
jerry <jerry.genser@indico.io>
2020-02-23 11:41:48 -0500

    upload file from path or bytes based on upload client

--- indicoio/client/storage.py
+++ indicoio/client/storage.py
@@ -1,11 +1,15 @@
 from indicoio.client.client import RequestProxy
 from typing import List
+from pathlib import Path
+import uuid
 
 
 class StorageClient(RequestProxy):
-    def upload_files(self, paths: List[str]):
-        files = {
-            path: open(path, "rb")
-            for path in paths
-        }
+    def upload_files(self, data: List[str]):
+        files = {}
+        for datum in data:
+            if Path(datum).exists():
+                files[datum] = open(datum, "rb")
+            else:
+                files[str(uuid.uuid4())] = datum
         return self.post("/api/storage/files/upload?upload_type=user", files=files)
