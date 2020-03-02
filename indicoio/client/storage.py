baf0d1f779b01e45b7b99068d22727cabfd42bdc
Chris Lee <sihrc.c.lee@gmail.com>
2020-03-02 12:21:04 -0500

    Jerry/docex (#9)
    * move file input parsing into storage client
    
    * move file input parsing into storage cient
    
    Co-authored-by: Jerry Genser <jerrygenser@gmail.com>

--- indicoio/client/storage.py
+++ indicoio/client/storage.py
@@ -2,6 +2,7 @@ from indicoio.client.client import RequestProxy
 from typing import List
 from pathlib import Path
 import uuid
+import simplejson as json
 
 
 class StorageClient(RequestProxy):
@@ -13,4 +14,19 @@ class StorageClient(RequestProxy):
                 files[path.stem] = path.open("rb")
             else:
                 files[str(uuid.uuid4())] = datum
-        return self.post("/api/storage/files/store", files=files)
+            
+        uploaded_files = self.post("/api/storage/files/store", files=files)
+
+        return _parse_uploaded_files(uploaded_files)
+
+
+def _parse_uploaded_files(uploaded_files: List[dict]):
+    return [
+        {
+            "filename": f["name"],
+            "filemeta": json.dumps(
+                {"path": f["path"], "name": f["name"], "uploadType": f["upload_type"]}
+            ),
+        }
+        for f in uploaded_files
+    ]
