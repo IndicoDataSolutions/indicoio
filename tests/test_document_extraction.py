49cb9acc5964eceee55e0afac02a666e86f8ccf3
jerry <jerry.genser@indico.io>
2020-03-06 13:16:09 -0500

    remove ipdb

--- tests/test_document_extraction.py
+++ tests/test_document_extraction.py
@@ -11,7 +11,6 @@ def indicoapi(request):
 def test_document_extraction_file(indicoapi):
     path = Path(__file__).parent / "data" / "mock.pdf"
     results = indicoapi.document_extraction(data=[path])
-    import ipdb; ipdb.set_trace()
     assert isinstance(results, list)
     assert isinstance(results[0], dict)
     for field in ("page_num", "size", "doc_offset", "image", "text"):
