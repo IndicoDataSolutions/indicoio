6a2189d298d108b19611ec8c5f96743be955ed3e
jerry <jerry.genser@indico.io>
2020-03-06 12:38:04 -0500

    update test

--- tests/test_document_extraction.py
+++ tests/test_document_extraction.py
@@ -11,7 +11,9 @@ def indicoapi(request):
 def test_document_extraction_file(indicoapi):
     path = Path(__file__).parent / "data" / "mock.pdf"
     results = indicoapi.document_extraction(data=[path])
+    import ipdb; ipdb.set_trace()
     assert isinstance(results, list)
     assert isinstance(results[0], dict)
-    for field in ("metadata", "pages"):
-        assert field in results[0]
+    for field in ("page_num", "size", "doc_offset", "image", "text"):
+        for page in results[0]['pages']:
+            assert page[field] is not None
